107749d7a5a3e22a6c368f1ac9162ecb00a1515b
Mubeen <mmcomputerstore@gmail.com>
2026-08-07 09:50:58 +0500

    fixed bug in database connection

--- Inventory Management.py
+++ Inventory Management.py
@@ -1,5 +1,23 @@
+import os
+from functools import wraps
+from dotenv import load_dotenv
+import mysql.connector as sql
+load_dotenv()
+def connectdatabase(fx,*args,**kwargs):
+    @wraps(fx)
+    def wraper(fx,*args,**kwargs):
+        db = sql.connect(host="localhost",user =os.getenv("user"),password = os.getenv("password"),database=os.getenv("database"))
+        cursor = db.cursor()
+        result = fx(*args,**kwargs)
+        cursor.close()
+        db.close()
+        return result
+    return wraper
+
+
 class Menu:
     def __init__(self):
+        self.product= Addproduct()
         while True:
             print("=== Menu ===\n1. Add Product")
             try:
