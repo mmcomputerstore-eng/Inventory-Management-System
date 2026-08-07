ca91d84582630dc4d4cf94296dc2bebda8a39364
Mubeen <mmcomputerstore@gmail.com>
2026-08-07 10:18:52 +0500

    Prevent duplicate products

--- Inventory Management.py
+++ Inventory Management.py
@@ -6,12 +6,12 @@ load_dotenv()
 def connectdatabase(fx):
     @wraps(fx)
     def wraper(self,*args,**kwargs):
-        db = sql.connect(host="localhost",user =os.getenv("user"),password = os.getenv("password"),database=os.getenv("database"))
-        self.cursor = db.cursor()
+        self.db = sql.connect(host="localhost",user =os.getenv("user"),password = os.getenv("password"),database=os.getenv("database"))
+        self.cursor = self.db.cursor()
         result = fx(self,*args,**kwargs)
-        db.commit()
+        self.db.commit()
         self.cursor.close()
-        db.close()
+        self.db.close()
         return result
     return wraper
 
@@ -31,19 +31,25 @@ class Addproduct:
     @connectdatabase
     def Add(self):
         name = input("Eanter Product Name : ")
-        while True:
-            try:
-                price = int(input(f"Eanter {name} Price : "))
-                break
-            except ValueError:
-                print("Please Eanter Price in Numbers...")
-        while True:
-            try:
-                stock = int(input(f"Eanter {name} Stock / Quantity : "))
-                break
-            except ValueError:
-                print("Please Eanter Stock in Numbers...")
-        self.cursor.execute("insert into products (product_name,Purchase_Price,Crunt_Stock) values (%s,%s,%s)",(name,price,stock))
+        self.cursor.execute("select * from products where product_name = %s",(name,))
+        data = self.cursor.fetchone()
+        if data ==None:
+            while True:
+                try:
+                    price = int(input(f"Eanter {name} Price : "))
+                    break
+                except ValueError:
+                    print("Please Eanter Price in Numbers...")
+            while True:
+                try:
+                    stock = int(input(f"Eanter {name} Stock / Quantity : "))
+                    break
+                except ValueError:
+                    print("Please Eanter Stock in Numbers...")
+            self.cursor.execute("insert into products (product_name,Purchase_Price,Crunt_Stock) values (%s,%s,%s)",(name,price,stock))
+            print(f"Product Name {name} Add successfully in Database...")
+        else:
+            print(f"product Name {name} Already Found in Record with product ID {data[0]} Dublicate Product Not Allowed...")
 
 
 
