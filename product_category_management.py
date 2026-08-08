ff1bddf5c63926adb93963bed806c039a63767bb
Mubeen <mmcomputerstore@gmail.com>
2026-08-08 11:29:21 +0500

    menu in product catagory

---
+++ product_category_management.py
@@ -0,0 +1,16 @@
+def menu(self):
+    while True:
+        print("=== Catagory Management ===\n1. Add Catagory\n2. View Catagorys\n3. Delete Catagory")
+        try:
+            option = int(input("Eanter Option Number to Select : "))
+            if option == 1 :
+                pass
+            elif option == 2:
+                pass
+            elif option == 3:
+                pass
+            else:
+                print("Please Eanter Write Option Number (1) for Add Catagory (2) for View Catagorys...")
+        except:
+            print("Please Eanter Option Number (1) for Add Catagory (2) for View Catagorys...")
+
