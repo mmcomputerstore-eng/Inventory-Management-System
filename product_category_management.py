74769b748d6a36dd19ba2f0fc8f795556a3c76c6
Mubeen <mmcomputerstore@gmail.com>
2026-08-08 11:32:59 +0500

    add (exit to main menu) option in Catagory managemnt menu

--- product_category_management.py
+++ product_category_management.py
@@ -1,6 +1,6 @@
 def menu(self):
     while True:
-        print("=== Catagory Management ===\n1. Add Catagory\n2. View Catagorys\n3. Delete Catagory")
+        print("=== Catagory Management ===\n1. Add Catagory\n2. View Catagorys\n3. Delete Catagory\n4. Exit to Main Menu")
         try:
             option = int(input("Eanter Option Number to Select : "))
             if option == 1 :
@@ -9,6 +9,9 @@ def menu(self):
                 pass
             elif option == 3:
                 pass
+            elif option == 4:
+                print("Exit Successfull...")
+                break
             else:
                 print("Please Eanter Write Option Number (1) for Add Catagory (2) for View Catagorys...")
         except:
