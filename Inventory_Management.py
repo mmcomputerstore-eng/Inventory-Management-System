dde698a866bab9bf65e85a953c1322713c53c302
Mubeen <mmcomputerstore@gmail.com>
2026-08-09 16:34:40 +0500

    add export in main menu

--- Inventory_Management.py
+++ Inventory_Management.py
@@ -9,12 +9,13 @@ from purchase import purchase
 from coustomers import customers_menu
 from sales import sales
 from reports import reports_menu
+from Export import export
 
 class Menu:
     def run(self):
         self.product= ProductManager()
         while True:
-            print("=== Menu ===\n1. Add Product\n2. View All Products\n3. Search Products\n4. Update Product\n5. Delete Product\n6. Catagory Management\n7. Supplier Management\n8. Purchase\n9. Coustomer Management\n10. Sale\n11. Reports\n12. Exit")
+            print("=== Menu ===\n1. Add Product\n2. View All Products\n3. Search Products\n4. Update Product\n5. Delete Product\n6. Catagory Management\n7. Supplier Management\n8. Purchase\n9. Coustomer Management\n10. Sale\n11. Reports\n12. Export\n13. Exit")
             try:
                 choice = int(input("Enter option Number to contineu : "))
                 if choice == 1:
@@ -40,6 +41,8 @@ class Menu:
                 elif choice ==11:
                     reports_menu(self)
                 elif choice == 12:
+                    export(self)
+                elif choice == 13:
                     print("Exit Successfull...")
                     break
             except ValueError:
