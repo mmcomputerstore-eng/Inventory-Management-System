fad6563b1dd13b741aff48ef1086041f747f5d7a
Mubeen <mmcomputerstore@gmail.com>
2026-08-09 09:07:53 +0500

    add exit option in main menu

--- Inventory_Management.py
+++ Inventory_Management.py
@@ -8,7 +8,7 @@ class Menu:
     def run(self):
         self.product= ProductManager()
         while True:
-            print("=== Menu ===\n1. Add Product\n2. View All Products\n3. Search Products\n4. Update Product\n5. Delete Product\n6. Catagory Management")
+            print("=== Menu ===\n1. Add Product\n2. View All Products\n3. Search Products\n4. Update Product\n5. Delete Product\n6. Catagory Management\n7. Exit")
             try:
                 choice = int(input("Enter option Number to contineu : "))
                 if choice == 1:
@@ -23,6 +23,9 @@ class Menu:
                     delete_product(self)
                 elif choice == 6:
                     menu(self)
+                elif choice == 7:
+                    print("Exit Successfull...")
+                    break
             except ValueError:
                 print("Please Eanter option Number like 1 for Add Product ...")
 class ProductManager:
